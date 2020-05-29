3ebd0cddd82071f68cb6207f5683b2472d94c3c2
Jam Risser <jam@codejam.ninja>
2020-05-29 02:23:50 +0000

    Updated entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ if [ -z "$POSTGRES_URL" ]; then
   export POSTGRES_URL=postgresql://$POSTGRES_USERNAME:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DATABASE?sslmode=$POSTGRES_SSLMODE
 fi
 
-node_modules/.bin/prisma2 generate
+node_modules/.bin/prisma generate
 
 echo "waiting for postgres . . ."
 until psql "$POSTGRES_URL" -c '\l' >/dev/null; do
