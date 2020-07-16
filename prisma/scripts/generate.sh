278ed2e0dc66d6b7547db6a5c0412fc25229baaf
Jam Risser <jam@codejam.ninja>
2020-07-16 09:51:55 +0000

    Fixed source error

--- prisma/scripts/generate.sh
+++ prisma/scripts/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-source prisma/scripts/dotenv.sh
+. prisma/scripts/dotenv.sh
 echo "$POSTGRES_URL"
 if [ -z "$POSTGRES_URL" ]; then
   export POSTGRES_URL=postgresql://$POSTGRES_USERNAME:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DATABASE?sslmode=$POSTGRES_SSLMODE
