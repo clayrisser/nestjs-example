46f0dbefc70049ddef49e4b8d37ebcf9b1d64f8a
Clay Risser <email@clayrisser.com>
2021-01-16 03:14:46 -0600

    Small improvements

--- src/modules/count/count.controller.ts
+++ src/modules/count/count.controller.ts
@@ -3,11 +3,9 @@ import { Public, Resource, Roles, Scopes } from 'nestjs-keycloak';
 import { Request } from 'express';
 import { SessionData } from '~/types';
 
-@Resource('app')
-@Controller()
+@Resource('count')
+@Controller('count')
 export class CountController {
-  constructor() {}
-
   @Get()
   @Public()
   @Scopes('hello', 'world', 'yip', 'yap')
@@ -16,6 +14,7 @@ export class CountController {
     return { message: 'Hello, world!' };
   }
 
+  @Roles('admin')
   @Get('/count')
   getCount(@Req() _req: Request, @Session() session: SessionData): number {
     session.count = ++session.count || 0;
