845c4ebbaf6f29fa473c0a522af9c41bac5ff3b6
Clay Risser <email@clayrisser.com>
2021-04-25 21:21:18 -0500

    Updated graphback module

--- src/modules/graphback/graphback.provider.ts
+++ src/modules/graphback/graphback.provider.ts
@@ -1,10 +1,11 @@
-// import { MongoClient } from 'mongodb';
-// import { createMongoDbProvider } from '@graphback/runtime-mongo';
 import Knex from 'knex';
 import path from 'path';
+import { ConfigService } from '@nestjs/config';
 import { FactoryProvider } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
 import { buildGraphbackAPI, GraphbackAPI } from 'graphback';
 import { createKnexDbProvider } from '@graphback/runtime-knex';
+import { createMongoDbProvider } from '@graphback/runtime-mongo';
 import { migrateDB, removeNonSafeOperationsFilter } from 'graphql-migrations';
 import { readFileSync } from 'fs';
 
@@ -14,43 +15,65 @@ export const GRAPHBACK = 'GRAPHBACK';
 
 const GraphbackProvider: FactoryProvider<Promise<GraphbackAPI>> = {
   provide: GRAPHBACK,
-  useFactory: async () => {
-    // const URL = 'mongodb://localhost:27017';
-    // const DATABASE = 'todo';
-    // const client = await MongoClient.connect(URL, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true
-    // });
-    // const db = client.db(DATABASE);
-    // const dataProviderCreator = createMongoDbProvider(db);
+  inject: [ConfigService],
+  useFactory: async (config: ConfigService) => {
+    const { dataProviderCreator, migrate } = await getDatabaseEngine(config);
+    const model = readFileSync(
+      path.resolve(__dirname, '../../../model/datamodel.graphql'),
+      'utf8'
+    );
+    const result = buildGraphbackAPI(model, {
+      dataProviderCreator
+    });
+    if (migrate) {
+      await migrate(result);
+      logger.log('Migrated database');
+    }
+    return result;
+  }
+};
+
+export async function getDatabaseEngine(config: ConfigService) {
+  const databaseEngine = config.get('DATABASE_ENGINE') as DatabaseEngine;
+  if (databaseEngine === DatabaseEngine.Mongo) {
+    const URL = `mongodb://${config.get('MONGO_HOST')}:${config.get(
+      'MONGO_PORT'
+    )}`;
+    const client = await MongoClient.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    const db = client.db(config.get('MONGO_DATABASE'));
+    const dataProviderCreator = createMongoDbProvider(db);
+    return { dataProviderCreator };
+  }
+  if (databaseEngine === DatabaseEngine.Postgres) {
     const dbConfig = {
       client: 'pg',
       connection: {
-        user: 'postgres',
-        password: 'postgres',
-        database: 'postgres',
-        host: 'localhost',
-        port: Number(process.env.DB_PORT || 5432)
+        user: config.get('POSTGRES_USER'),
+        password: config.get('POSTGRES_PASSWORD'),
+        database: config.get('POSTGRES_DATABASE'),
+        host: config.get('POSTGRES_HOST'),
+        port: Number(config.get('POSTGRES_PORT') || 5432)
       },
       pool: { min: 5, max: 30 }
     };
     const db = Knex(dbConfig);
     const dataProviderCreator = createKnexDbProvider(db);
-
-    const userModel = readFileSync(
-      path.resolve(__dirname, '../../../model/datamodel.graphql'),
-      'utf8'
-    );
-    const result = buildGraphbackAPI(userModel, {
-      dataProviderCreator
-    });
-    migrateDB(dbConfig, result.typeDefs, {
-      operationFilter: removeNonSafeOperationsFilter
-    }).then(() => {
-      logger.log('Migrated database');
-    });
-    return result;
+    async function migrate({ typeDefs }: GraphbackAPI) {
+      await migrateDB(dbConfig, typeDefs, {
+        operationFilter: removeNonSafeOperationsFilter
+      });
+    }
+    return { dataProviderCreator, migrate };
   }
-};
+  throw new Error(`database engine '${databaseEngine}' is invalid`);
+}
+
+export enum DatabaseEngine {
+  Mongo = 'mongo',
+  Postgres = 'postgres'
+}
 
 export default GraphbackProvider;
